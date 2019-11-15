c5a09f2728b333590206dbf45b8a2e167cb03341
Lela Smith <smith.lelaa@gmail.com>
2019-11-15 11:09:01 -0800

    pass first spec of calculating earth age in years from birthdate input

--- __tests__/galacticAge.test.js
+++ __tests__/galacticAge.test.js
@@ -1,8 +1,10 @@
 import { GalacticAge } from './../src/backend.js';
 
+
 describe ('GalacticAge', () => {
-  test('should correctly determine users age in earth from birthdate input' => {
+  test('should correctly determine users age in earth from birthdate input', () => {
     const newGalacticAge = new GalacticAge("September 19, 1987");
-    expect(newGalacticAge).toEqual(32);
+    newGalacticAge.calculateAge("September 19, 1987");
+    expect(newGalacticAge.age).toEqual(32);
   });
 });
--- src/backend.js
+++ src/backend.js
@@ -5,5 +5,15 @@ export function GalacticAge(birthdate) {
 }
 
 GalacticAge.prototype.calculateAge = function(birthdate) {
-  
+  let dateParseMethod = Date.parse(birthdate);
+  let dateNowMethod = Date.now();
+  let ageInMilliseconds = dateNowMethod - dateParseMethod;
+  let second = 1000;
+  let minute = second*60;
+  let hour = minute*60;
+  let day = hour*24;
+  var year = day*365;
+  var ageInYears = Math.round(ageInMilliseconds/year);
+  this.age = ageInYears;
+  return this.age;
 };

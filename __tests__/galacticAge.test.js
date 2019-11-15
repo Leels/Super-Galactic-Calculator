a2ac6ed2074bbbf05810b25a34bf9e033c2e90cd
Lela Smith <smith.lelaa@gmail.com>
2019-11-15 13:27:07 -0800

    add third spec to calculate age in venus years

--- __tests__/galacticAge.test.js
+++ __tests__/galacticAge.test.js
@@ -7,9 +7,16 @@ describe ('GalacticAge', () => {
     newGalacticAge.calculateEarthAge("September 19, 1987");
     expect(newGalacticAge.earthAge).toEqual(32);
   });
+
   test('should correctly determine users age in mercury years from birthdate input', () => {
     const newGalacticAge = new GalacticAge("September 19, 1987");
     newGalacticAge.calculateMercuryAge("September 19, 1987");
     expect(newGalacticAge.mercuryAge).toEqual(133);
-    });
+  });
+
+  test('should correctly determine users age in venus years from birthdate input', () => {
+    const newGalacticAge = new GalacticAge("September 19, 1987");
+    newGalacticAge.calculateVenusAge("September 19, 1987");
+    expect(newGalacticAge.mercuryAge).toEqual(52);
+  });
 });

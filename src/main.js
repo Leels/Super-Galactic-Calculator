22d942630e3a4c21af51889a683b0fc2e38dcfd9
Lela Smith <smith.lelaa@gmail.com>
2019-11-16 19:40:39 -0800

    add styling to website. add spaces for planets saturn, neptune, uranus and pluto.

--- src/main.js
+++ src/main.js
@@ -9,6 +9,35 @@ $(document).ready(function() {
     event.preventDefault();
     const birthdate = $("input#birthdate").val();
     const newGalacticAge = new GalacticAge(birthdate);
+
+    $("#mercury").click(function() {
+    $("#mercury p").toggle();
+  });
+  $("#venus").click(function() {
+    $("#venus p").toggle();
+  });
+  $("#earth").click(function() {
+    $("#earth p").toggle();
+  });
+  $("#mars").click(function() {
+    $("#mars p").toggle();
+  });
+  $("#jupiter").click(function() {
+    $("#jupiter p").toggle();
+  });
+  $("#saturn").click(function() {
+    $("#saturn p").toggle();
+  });
+  $("#uranus").click(function() {
+    $("#uranus p").toggle();
+  });
+  $("#neptune").click(function() {
+    $("#neptune p").toggle();
+  });
+  $("#pluto").click(function() {
+    $("#pluto p").toggle();
+  });
+
     $("#result").show();
     $("#earth-years").text(newGalacticAge.calculateEarthAge(birthdate));
     $("#mercury-years").text(newGalacticAge.calculateMercuryAge(birthdate));
